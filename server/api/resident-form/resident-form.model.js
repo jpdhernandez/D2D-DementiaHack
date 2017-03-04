8ac54e366605bc02fab4e5e8e9f82bddffa1de02
Rohit Nair <rohitnair416@gmail.com>
2017-03-04 15:31:13 -0500

    created resident-form-model

--- server/api/resident-form/resident-form.model.js
+++ server/api/resident-form/resident-form.model.js
@@ -4,9 +4,22 @@ import mongoose from 'mongoose';
 import {registerEvents} from './resident-form.events';
 
 var ResidentFormSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  Name: String,
+  "Date of birth": String,
+  "Suite #": String,
+  "Date Form Completed": String,
+  "Form Completed By (Resident of Name of Other)": String,
+  "Move In Date": String,
+  Identity : {
+    "Where did you grow up" : String,
+    "Did you belong to any groups or clubs? (volunteering, service clubs, sports leagues, etc)" : String,
+    "My favourite things include...(pets, foods, drinks etc.)" : String
+  },
+  Autonomy : {
+    "What time do you prefer to get up?": String,
+    "When do you like to go to bed?  What do your night-time rituals or routines include?": String
+  },
+  completed: { type: Boolean, default: false } //flag : is the form completed starts with false
 });
 
 registerEvents(ResidentFormSchema);
