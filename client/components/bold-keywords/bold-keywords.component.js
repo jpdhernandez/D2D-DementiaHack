cf3f0a2829da5d91f7faa4589860abc5aa487fb5
Mohammed Buttu <mohammedbuttu@gmail.com>
2017-03-05 02:06:35 -0500

    Update colours for bold-keywords

--- client/components/bold-keywords/bold-keywords.component.js
+++ client/components/bold-keywords/bold-keywords.component.js
@@ -1,8 +1,10 @@
 'use strict';
 const angular = require('angular');
 
-const extractor = require('keyword-extractor');
-const _ = require('lodash');
+import extractor from 'keyword-extractor';
+import _ from 'lodash';
+// import parseColour from 'parse-color';
+import wordsToNumbers from 'words-to-numbers';
 
 import './bold-keywords.css';
 
@@ -20,26 +22,58 @@ export class boldKeywordsComponent {
     this.words = _.map(split, word => {
       const cleaned = word.split('')
       .filter(char => !_.includes([';', ',', '.'], char))
-      .join('');
+      .join('')
+      .toLowerCase();
 
-      const isKeyword = _.includes(keywords, cleaned.toLowerCase());
+      const isKeyword = _.includes(keywords, cleaned);
 
       const isNice = _.includes(niceWords, cleaned);
       const isBad = _.includes(badWords, cleaned);
 
-      let cssClass = "normal-word";
+      let cssClass = 'normal-word';
 
       if (isNice) {
-        cssClass = "good-word";
+        if (cleaned === 'love') {
+          word = '';
+          cssClass = 'heart-icon fa fa-heart';
+        }  else {
+          cssClass = 'good-word';
+        }
       } else if (isBad) {
-        cssClass = "bad-word";
+        cssClass = 'bad-word';
       } else if (isKeyword) {
-        cssClass = "big-word";
+        // if (cleaned === 'bath') {
+        //   cssClass = 'good-word fa fa-bathtub';
+        //   word = '';
+        // } else {
+          cssClass = 'big-word';
+        // }
+      }
+
+      let style = {};
+      //
+      // const parsedStyle = parseColour(cleaned);
+
+      // if (parsedStyle.hex) {
+      //   style = {
+      //     color: parsedStyle.hex
+      //   };
+      // }
+
+      const parsedNumber = wordsToNumbers(cleaned);
+
+      // For some reason wordsToNumbers parses an 'a' into a 1
+      if (cleaned !== 'a' && parsedNumber) {
+        cssClass = 'number-word';
+        word = parsedNumber;
+      } else if (_.isNumber(+cleaned) && !_.isNaN(+cleaned)) {
+        cssClass = 'number-word';
       }
 
       return {
         cssClass,
-        word
+        word,
+        style
       };
     });
   }
